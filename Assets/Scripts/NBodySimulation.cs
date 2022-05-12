4106b4b6e4cba1e00f19e9af58f916850b42d59a
Hosu~NyanPasu <vortex.k1@gmail.com>
2022-05-12 19:04:15 +0530

    Initial commit

---
+++ Assets/Scripts/NBodySimulation.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NBodySimulation : MonoBehaviour
+{
+    public CelestialBody[] bodies;
+    static NBodySimulation instance;
+    public const float physicsTimeStep = 0.01f;
+    public const float gravitationalConstant = 0.0001f;
+
+    void Awake()
+    {
+
+        bodies = FindObjectsOfType<CelestialBody>();
+        Time.fixedDeltaTime = physicsTimeStep;
+        Debug.Log("Setting fixedDeltaTime to: " + physicsTimeStep);
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < bodies.Length; i++)
+        {
+            Vector3 acceleration = CalculateAcceleration(bodies[i].Position, bodies[i]);
+            bodies[i].UpdateVelocity(acceleration, physicsTimeStep);
+            //bodies[i].UpdateVelocity (bodies, Universe.physicsTimeStep);
+        }
+
+        for (int i = 0; i < bodies.Length; i++)
+        {
+            bodies[i].UpdatePosition(physicsTimeStep);
+        }
+
+    }
+
+    public static Vector3 CalculateAcceleration(Vector3 point, CelestialBody ignoreBody = null)
+    {
+        Vector3 acceleration = Vector3.zero;
+        foreach (var body in Instance.bodies)
+        {
+            if (body != ignoreBody)
+            {
+                float sqrDst = (body.Position - point).sqrMagnitude;
+                Vector3 forceDir = (body.Position - point).normalized;
+                acceleration += forceDir * gravitationalConstant * body.mass / sqrDst;
+            }
+        }
+
+        return acceleration;
+    }
+
+    public static CelestialBody[] Bodies
+    {
+        get
+        {
+            return Instance.bodies;
+        }
+    }
+
+    static NBodySimulation Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<NBodySimulation>();
+            }
+            return instance;
+        }
+    }
+}
